037780f8938bc253325bdd2b64a28559b939efcc
Priscila <priscilapintado66@gmail.com>
2021-09-07 23:15:08 -0400

    added landing page

---
+++ client/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import GridList from '@material-ui/core/GridList'
+const LandingPage = () => {
+  return (
+    <div className="landingBackground">
+      <Typography variant="h1">You are entering Priscila's space</Typography>
+    </div>
+  )
+}
+export default LandingPage
